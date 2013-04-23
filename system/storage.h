aa7286cf33ff668721219c06ca4d9c3f56c4818d
pichenettes <ol.gillet@gmail.com>
2013-04-24 01:01:08 +0200

    Added parsimonious mode to storage class

--- system/storage.h
+++ system/storage.h
@@ -24,7 +24,21 @@
 //
 // -----------------------------------------------------------------------------
 //
-// Helper functions for using the last page of flash for non-volatile storage.
+// Helper functions for using the last pages of flash for non-volatile storage.
+//
+// Because the flash memory has a limited number of erase cycles (10k to 100k)
+// using flash as a permanent storage space is fine for calibration or presets,
+// but should be handled with care when saving, for example, the state of a
+// device - which can be updated every second!
+//
+// If the amount of data to save is small, it is more efficient to just append
+// versions upon versions after each other. For example, if the buffer to save
+// is 16 bytes long, it is recommended to erase a page, and fill it
+// progressively at each save request with the blocks of data until it is full
+// and the page can be erased again. This way, only 1 erase will be needed
+// every 64th call to the save function. This strategy is implemented in
+// ParsimoniousLoad and ParsimoniousSave, and practically extends the life of
+// the flash memory life by a 40x factor in Braids.
 
 #ifndef STMLIB_SYSTEM_STORAGE_H_
 #define STMLIB_SYSTEM_STORAGE_H_
@@ -54,28 +68,9 @@ class Storage {
   }
 
   static void Save(const void* data, size_t data_size, uint8_t page_index) {
-    uint32_t base = FLASH_STORAGE_BASE + page_index * PAGE_SIZE;
-    
     FLASH_Unlock();
-    FLASH_ErasePage(base);
-    
-    // Write argument.
-    const uint32_t* words = (const uint32_t*)(data);
-    size_t size = data_size;
-    uint32_t address = base;
-    while (size >= 4) {
-      FLASH_ProgramWord(address, *words++);
-      address += 4;
-      size -= 4;
-    }
-    // If the size is not a multiple of 4, write tail.
-    if (size) {
-      FLASH_ProgramWord(address, *(const uint16_t*)(words));
-    }
-    
-    // Write checksum.
-    uint16_t checksum = Checksum(data, data_size);
-    FLASH_ProgramHalfWord(base + data_size, checksum);
+    FLASH_ErasePage(FLASH_STORAGE_BASE + page_index * PAGE_SIZE);
+    WriteBlock(FLASH_STORAGE_BASE + page_index * PAGE_SIZE, data, data_size);
   };
   
   template<typename T>
@@ -89,13 +84,98 @@ class Storage {
   }
 
   static bool Load(void* data, size_t data_size, uint8_t page_index) {
-    uint32_t base = FLASH_STORAGE_BASE + page_index * PAGE_SIZE;    
+    uint32_t base = FLASH_STORAGE_BASE + page_index * PAGE_SIZE;
     memcpy(data, (void*)(base), data_size);
     uint16_t checksum = (*(uint16_t*)(base + data_size));
     return checksum == Checksum(data, data_size);
   };
   
+  template<typename T>
+  static void ParsimoniousSave(const T& data, uint16_t* version_token) {
+    return ParsimoniousSave((void*)(&data), sizeof(T), version_token);
+  }
+  
+  static void ParsimoniousSave(
+      const void* data,
+      size_t data_size,
+      uint16_t* version_token) {
+    // 2 bytes of checksum and 2 bytes of version are added to the block.
+    size_t block_size = data_size + 2 + 2;
+    uint32_t start = FLASH_STORAGE_BASE + block_size * *version_token;
+    if (start + block_size >= last_address) {
+      // Restart a blank block.
+      *version_token = 0;
+      start = FLASH_STORAGE_BASE;
+    }
+    FLASH_Unlock();
+
+    // If we will write into a new page, erase it.
+    uint32_t previous_page = start - 1;
+    previous_page -= previous_page % PAGE_SIZE;
+    uint32_t this_page = start + block_size;
+    this_page -= this_page % PAGE_SIZE;
+    if (this_page != previous_page) {
+      FLASH_ErasePage(this_page);
+    }
+    
+    WriteBlock(start, data, data_size);
+    FLASH_ProgramHalfWord(start + data_size + 2, *version_token);
+    *version_token = *version_token + 1;
+  }
+  
+  template<typename T>
+  static bool ParsimoniousLoad(T* data, uint16_t* version_token) {
+    return ParsimoniousLoad((void*)(data), sizeof(T), version_token);
+  }
+  
+  static bool ParsimoniousLoad(
+      void* data,
+      size_t data_size,
+      uint16_t* version_token) {
+    size_t block_size = data_size + 2 + 2;
+
+    // Try from the end of the reserved area until we find a block with 
+    // the right checksum and the right version index. 
+    for (int16_t candidate_version = (num_pages * PAGE_SIZE / block_size) - 1;
+         candidate_version >= 0; 
+         --candidate_version) {
+      uint32_t start = FLASH_STORAGE_BASE + candidate_version * block_size;
+      
+      memcpy(data, (void*)(start), data_size);
+      uint16_t expected_checksum = Checksum(data, data_size);
+      uint16_t read_checksum = (*(uint16_t*)(start + data_size));
+      uint16_t version_number = (*(uint16_t*)(start + data_size + 2));
+      if (read_checksum == expected_checksum &&
+          version_number == candidate_version) {
+        *version_token = version_number + 1;
+        return true;
+      }
+    }
+    // Memory appears to be corrupted or virgin - restart from scratch.
+    *version_token = 0;
+    return false;
+  }
+  
  private:
+  static void WriteBlock(uint32_t start, const void* data, size_t data_size) {
+    const uint32_t* words = (const uint32_t*)(data);
+    size_t size = data_size;
+    uint32_t address = start;
+    while (size >= 4) {
+      FLASH_ProgramWord(address, *words++);
+      address += 4;
+      size -= 4;
+    }
+    // If the size is not a multiple of 4, write tail.
+    if (size) {
+      FLASH_ProgramWord(address, *words);
+    }
+    
+    // Write checksum.
+    uint16_t checksum = Checksum(data, data_size);
+    FLASH_ProgramHalfWord(start + data_size, checksum);
+  }
+   
   static uint16_t Checksum(const void* data, uint16_t size) {
     uint16_t s = 0;
     const uint8_t* d = static_cast<const uint8_t*>(data);

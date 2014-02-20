47e152535aea365e4268d296b4b426505c196b1b
olivier gillet <ol.gillet@gmail.com>
2014-02-20 04:14:23 +0100

    Extra method signature to do FFTs on sizes smaller than allocated

--- fft/shy_fft.h
+++ fft/shy_fft.h
@@ -30,6 +30,9 @@
 // Improvements:
 // * No dynamic allocations.
 // * No additional buffering (can use the input buffer as a workspace).
+// * No big bitrev lookup table.
+// * Keep the fixed size template signature, but also provide method for
+//   variable size (up to the fixed size).
 
 #ifndef STMLIB_FFT_SHY_FFT_H_
 #define STMLIB_FFT_SHY_FFT_H_
@@ -309,6 +312,105 @@ struct DirectTransform {
       std::copy(&d[0], &d[size], &output[0]);
     }
   }
+  
+  // The exact same thing but with "num_passes" as a run-time argument.
+  void operator()(
+      const T* input,
+      T* output,
+      T* workspace,
+      const uint8_t* bit_rev,
+      Phasor* phasor,
+      size_t rt_num_passes) {
+    T* s;
+    T* d;
+    Math<T> math;
+    size_t rt_size = 1 << rt_num_passes;
+    // First and second pass.
+    d = output;
+    for (size_t i = 0; i < rt_size; i += 4) {
+      const T* s = input;
+      size_t r0 = \
+          ((bit_rev[i & 0xff] << 8) | bit_rev[i >> 8]) >> (16 - rt_num_passes);
+      size_t r1 = r0 + 2 * (rt_size >> 2);
+      size_t r2 = r0 + 1 * (rt_size >> 2);
+      size_t r3 = r0 + 3 * (rt_size >> 2);
+      
+      d[1] = s[r0] - s[r1];
+      d[3] = s[r2] - s[r3];
+      T a = s[r0] + s[r1];
+      T b = s[r2] + s[r3];
+      d[0] = a + b;
+      d[2] = a - b;
+      d += 4;
+    }
+    
+    // Third pass.
+    s = output;
+    d = workspace;
+    for (size_t i = 0; i < rt_size; i += 8) {
+      T v;
+
+      d[i] = s[i] + s[i + 4];
+      d[i + 4] = s[i] - s[i + 4];
+      d[i + 2] = s[i + 2];
+      d[i + 6] = s[i + 6];
+
+      v = (s[i + 5] - s[i + 7]) * math.sqrt_2_div_2();
+      d[i + 1] = s[i + 1] + v;
+      d[i + 3] = s[i + 1] - v;
+
+      v = (s[i + 5] + s[i + 7]) * math.sqrt_2_div_2();
+      d[i + 5] = v + s[i + 3];
+      d[i + 7] = v - s[i + 3];
+    }
+    
+    // Remaining passes.
+    for (size_t pass = 3; pass < rt_num_passes; ++pass) {
+      // Flip source and destination pointers
+      {
+        T* tmp = s;
+        s = d;
+        d = tmp;
+      }
+      
+      size_t n = 1 << pass;
+      size_t n_2 = n >> 1;
+
+      for (size_t i = 0; i < rt_size; i += (n << 1)) {
+        T* s1r = s + i;
+        T* s2r = s1r + n;
+        T* dr = d + i;
+        T* di = dr + n;
+
+        dr[0] = s1r[0] + s2r[0];
+        di[0] = s1r[0] - s2r[0];
+        dr[n_2] = s1r[n_2];
+        di[n_2] = s2r[n_2];
+        T* s1i = s1r + n_2;
+        T* s2i = s1i + n;
+        phasor->Start(pass);
+        for (size_t j = 1; j < n_2; ++j) {
+          T c = phasor->cos();
+          T s = phasor->sin();
+          T v;
+
+          v = s2r[j] * c - s2i[j] * s;
+          dr[j] = s1r[j] + v;
+          di[-j] = s1r[j] - v;
+
+          v = s2r[j] * s + s2i[j] * c;
+          di[j] = v + s1i[j];
+          di[n - j] = v - s1i[j];
+          phasor->Rotate();
+        }
+      }
+    }
+    
+    // Annoying additional data copy step.
+    if (d != output) {
+      std::copy(&d[0], &d[rt_size], &output[0]);
+    }
+  }
 };
 
 template<typename T, typename Phasor>
@@ -446,6 +548,107 @@ struct InverseTransform {
       s += 4;
     }
   }
+  
+  void operator()(
+      const T* input,
+      T* output,
+      T* workspace,
+      const uint8_t* bit_rev,
+      Phasor* phasor,
+      size_t rt_num_passes) {
+    T* s = (T*)(input);
+    T* d = output;
+    Math<T> math;
+    
+    size_t rt_size = 1 << rt_num_passes;
+    
+    // Remaining passes.
+    for (size_t pass = rt_num_passes - 1; pass >= 3; --pass) {
+      size_t n = 1 << pass;
+      size_t n_2 = n >> 1;
+      
+      for (size_t i = 0; i < rt_size; i += (n << 1)) {
+        T* sr = s + i;
+        T* si = sr + n;
+        T* d1r = d + i;
+        T* d2r = d1r + n;
+        
+        d1r[0] = sr[0] + si[0];
+        d2r[0] = sr[0] - si[0];
+        d1r[n_2] = sr[n_2] * T(2);
+        d2r[n_2] = si[n_2] * T(2);
+      
+        T* d1i = d1r + n_2;
+        T* d2i = d1i + n;
+        phasor->Start(pass);
+        for (size_t j = 1; j < n_2; ++j) {
+          d1r[j] = sr[j] + si[-j];
+          d1i[j] = si[j] - si[n - j];
+          
+          T c = phasor->cos();
+          T s = phasor->sin();
+          T vr = sr[j] - si[-j];
+          T vi = si[j] + si[n - j];
+          
+          d2r[j] = vr * c + vi * s;
+          d2i[j] = vi * c - vr * s;
+          phasor->Rotate();
+        }
+      }
+
+      // Flip source and destination pointers for the next pass.
+      if (d == output) {
+        s = output;
+        d = workspace;
+      } else {
+        s = workspace;
+        d = output;
+      }
+    }
+    
+    // Copy data if necessary.
+    if (d == output) {
+      std::copy(&s[0], &s[rt_size], &output[0]);
+    }
+    
+    s = output;
+    d = workspace;
+    for (size_t i = 0; i < rt_size; i += 8) {
+      T vr, vi;
+      d[i] = s[i] + s[i + 4];
+      d[i + 4] = s[i] - s[i + 4];
+      d[i + 2] = s[i + 2] * T(2);
+      d[i + 6] = s[i + 6] * T(2);
+      d[i + 1] = s[i + 1] + s[i + 3];
+      d[i + 3] = s[i + 5] - s[i + 7];
+      vr = s[i + 1] - s[i + 3];
+      vi = s[i + 5] + s[i + 7];
+      d[i + 5] = (vr + vi) * math.sqrt_2_div_2();
+      d[i + 7] = (vi - vr) * math.sqrt_2_div_2();
+    }
+    
+    // First and second pass.
+    s = workspace;
+    d = output;
+    for (size_t i = 0; i < rt_size; i += 4) {
+      size_t r0 = \
+            ((bit_rev[i & 0xff] << 8) | bit_rev[i >> 8]) >> (16 - rt_num_passes);
+      size_t r1 = r0 + 2 * (rt_size >> 2);
+      size_t r2 = r0 + 1 * (rt_size >> 2);
+      size_t r3 = r0 + 3 * (rt_size >> 2);
+      
+      T b_0 = s[0] + s[2];
+      T b_2 = s[0] - s[2];
+      T b_1 = s[1] * T(2);
+      T b_3 = s[3] * T(2);
+      
+      d[r0] = b_0 + b_1;
+      d[r1] = b_0 - b_1;
+      d[r2] = b_2 + b_3;
+      d[r3] = b_2 - b_3;
+      s += 4;
+    }
+  }
 };
 
 template<typename T, typename Phasor>
@@ -482,11 +685,15 @@ template<
     size_t size=16,
     template <typename, size_t> class Phasor=LutPhasor>
 class ShyFFT {
- private:
+ public:
   enum {
-    num_passes = Log2<size>::value
+    num_passes = Log2<size>::value,
+    max_size = size
   };
+
+ private:
   typedef Phasor<T, num_passes> PhasorType;
+
  public:
   ShyFFT() { }
   ~ShyFFT() { }
@@ -548,6 +755,51 @@ class ShyFFT {
         num_passes <= 8 ? &bit_rev_[0] : bit_rev_256_lut_,
         &phasor_);
   }
+  
+  void Direct(const T* input, T* output, T* workspace, size_t n) {
+    DirectTransform<T, num_passes, Phasor<T, num_passes> > d;
+    d(
+        input,
+        output,
+        workspace,
+        bit_rev_256_lut_,
+        &phasor_,
+        n);
+  }
+  
+  void Direct(T* input, T* output, size_t n) {
+    DirectTransform<T, num_passes, Phasor<T, num_passes> > d;
+    d(
+        input,
+        output,
+        input,
+        bit_rev_256_lut_,
+        &phasor_,
+        n);
+  }
+  
+  void Inverse(const T* input, T* output, T* workspace, size_t n) {
+    InverseTransform<T, num_passes, Phasor<T, num_passes> > i;
+    i(
+        input,
+        output,
+        workspace,
+        bit_rev_256_lut_,
+        &phasor_,
+        n);
+  }
+  
+  void Inverse(T* input, T* output, size_t n) {
+    InverseTransform<T, num_passes, Phasor<T, num_passes> > i;
+    i(
+        input,
+        output,
+        input,
+        bit_rev_256_lut_,
+        &phasor_,
+        n);
+  }
+  
 
  private:
   PhasorType phasor_;

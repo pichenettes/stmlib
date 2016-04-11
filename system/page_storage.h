3312dd7d4b72d2073d710f8cf482a47176142e17
Olivier Gillet <ol.gillet@gmail.com>
2016-04-11 09:41:03 +0200

    Port of ChunkStorage for STM32F1/F3

--- system/page_storage.h
+++ system/page_storage.h
@@ -56,6 +56,161 @@
 
 namespace stmlib {
 
+// Class for storing calibration and state data in a same sector of flash
+// without having to rewrite the calibration data every time the state is
+// changed.
+//
+// Data is stored in a RIFF-ish format, with the peculiarity that the size
+// field of the header is 16-bit instead of 32-bit - the remaining 16 bits being
+// used to store a naive checksum of the chunk data.
+// 
+// +----+-----------------+----+------------+----+------------+----+--
+// |HEAD|CALIBRATION CHUNK|HEAD|STATE CHUNK1|HEAD|STATE CHUNK2|HEAD|..
+// +----+-----------------+----+------------+----+------------+----+--
+//
+// The first chunk stores the large, "slow" data like calibration or presets.
+// Whenever this data needs to be saved, the entire sector of flash is erased.
+//
+// The subsequent chunks store successive revision of the "fast" data like
+// module state. Whenever this data changes, a new chunk is appended to the list
+// until the flash sector is filled - in which case the sector is erased, and
+// the calibration data + first version of state is written.
+template<
+    uint32_t flash_start,
+    uint32_t flash_end,
+    typename PersistentData,
+    typename StateData>
+class ChunkStorage {
+ private:
+  struct ChunkHeader {
+    uint32_t tag;
+    uint16_t size;
+    uint16_t checksum;
+  };
+
+  enum {
+    FLASH_STORAGE_SIZE = flash_end - flash_start
+  };
+
+ public:
+  ChunkStorage() { }
+  ~ChunkStorage() { }
+  // Loads the latest saved data. In case the sector is blank/corrupted,
+  // reinitializes the sector and returns false.
+  bool Init(PersistentData* persistent_data, StateData* state_data) {
+    persistent_data_ = persistent_data;
+    state_data_ = state_data;
+
+    if (ReadChunk(0, persistent_data)) {
+      for (next_state_chunk_index_ = 1;
+           chunk_address(next_state_chunk_index_ + 1) <= flash_end;
+           ++next_state_chunk_index_) {
+         if (!ReadChunk(next_state_chunk_index_, state_data)) {
+           break;
+         }
+      }
+      if (next_state_chunk_index_ != 1) {
+        return true;
+      }
+    }
+    Format();
+    return false;
+  }
+
+  void SaveState() {
+    if (chunk_address(next_state_chunk_index_ + 1) > flash_end) {
+      Format();
+    } else {
+      FLASH_Unlock();
+      WriteChunk(next_state_chunk_index_, state_data_);
+      next_state_chunk_index_++;
+    }
+  }
+
+  void SavePersistentData() {
+    Format();
+  }
+
+ private:
+   void Format() {
+    FLASH_Unlock();
+    for (uint32_t address = flash_start;
+         address < flash_end;
+         address += PAGE_SIZE) {
+      FLASH_ErasePage(address);
+    }
+    WriteChunk(0, persistent_data_);
+    WriteChunk(1, state_data_);
+    next_state_chunk_index_ = 2;
+  }
+
+  template<typename T>
+  bool ReadChunk(size_t index, T* data) {
+    const char* flash_ptr = (const char*)(chunk_address(index));
+    ChunkHeader* h = (ChunkHeader*)(flash_ptr);
+    if (h->tag == T::tag &&
+        h->size == sizeof(T) &&
+        Checksum(flash_ptr + sizeof(ChunkHeader), h->size) == h->checksum) {
+      memcpy(data, flash_ptr + sizeof(ChunkHeader), h->size);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  template<typename T>
+  void WriteChunk(size_t index, const T* data) {
+    ChunkHeader h;
+    h.tag = T::tag;
+    h.size = sizeof(T);
+    h.checksum = Checksum(data, sizeof(T));
+
+    FlashWrite(chunk_address(index), &h);
+    FlashWrite(chunk_address(index) + sizeof(ChunkHeader), data);
+  }
+
+  template<typename T>
+  void FlashWrite(uint32_t address, const T* data) {
+    const uint32_t* words = (const uint32_t*)(data);
+    size_t size = (sizeof(T) + 3) & ~0x03;
+    while (size) {
+      FLASH_ProgramWord(address, *words);
+      address += 4;
+      size -= 4;
+      ++words;
+    }
+  }
+
+  template<typename T>
+  inline size_t ChunkSize() {
+    return sizeof(ChunkHeader) + ((sizeof(T) + 3) & ~0x03);
+  }
+
+  uint32_t chunk_address(size_t index) {
+    if (index == 0) {
+      return flash_start;
+    } else {
+      return flash_start + ChunkSize<PersistentData>() + ChunkSize<StateData>() * (index - 1);
+    }
+  }
+
+  uint16_t Checksum(const void* data, size_t size) const {
+    uint16_t sum = 0;
+    const char* bytes = static_cast<const char*>(data);
+    while (size--) {
+      sum += *bytes++;
+    }
+    return sum ^ 0xffff;
+  }
+
+ private:
+  PersistentData* persistent_data_;
+  StateData* state_data_;
+  uint16_t next_state_chunk_index_;
+
+  DISALLOW_COPY_AND_ASSIGN(ChunkStorage);
+};
+
 template<uint32_t last_address = 0x8020000, uint16_t num_pages = 1>
 class Storage {
  public:

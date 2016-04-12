9954c7b0b6e2bfeac8e4e09fb6e9e21749dc1108
Olivier Gillet <ol.gillet@gmail.com>
2016-04-12 16:05:59 +0200

    Do not squeeze transients in limiter

--- dsp/limiter.h
+++ dsp/limiter.h
@@ -52,7 +52,7 @@ class Limiter {
       float s = *in_out * pre_gain;
       SLOPE(peak_, fabs(s), 0.05f, 0.00002f);
       float gain = (peak_ <= 1.0f ? 1.0f : 1.0f / peak_);
-      *in_out++ = stmlib::SoftLimit(s * gain * 0.8f);
+      *in_out++ = s * gain * 0.8f;
     }
   }
 
